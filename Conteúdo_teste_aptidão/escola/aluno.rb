423d5018e127e8e1350d373a3a3d2a05397b35de
UtescherIntrieri <18ianaui1999@gmail.com>
2023-09-26 13:39:08 -0300

    .

--- Conteúdo_teste_aptidão/escola/aluno.rb
+++ Conteúdo_teste_aptidão/escola/aluno.rb
@@ -1,7 +1,7 @@
 class Aluno
   attr_accessor :nome, :telefone, :matricula
 
-  def initialize(nome, telefone, matricula)
+  def initialize(nome,telefone, matricula)
     @nome = nome
     @telefone = telefone
     @matricula = matricula 
